44cbdc49ed4a90d8a8722bb006549db7d614e2d8
Mario te Best <mario.tebest@dccs.at>
2020-03-06 09:47:22 +0100

    build.ps1 debugging

--- src/Build.ps1
+++ src/Build.ps1
@@ -52,12 +52,16 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 EnsurePsbuildInstalled
 
-exec { & dotnet restore .\src\TransactionalFileManager.sln }
-exec { & dotnet build .\src\TransactionalFileManager.sln -c Release }
+gci | Write-Verbose -Verbose
+
+exec { & dotnet restore .\src }
+exec { & dotnet build .\src -c Release }
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 # exec { & dotnet test -c Release }
+
+
 
 exec { & dotnet pack .\src\ChinhDo.Transactions.FileManager -c Release -o .\artifacts --version-suffix=$revision }
