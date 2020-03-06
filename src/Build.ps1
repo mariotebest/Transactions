2f8c3e3620de193ff9af14ff698e4199016d9ac7
Mario te Best <mario.tebest@dccs.at>
2020-03-06 09:44:25 +0100

    appveyor

--- src/Build.ps1
+++ src/Build.ps1
@@ -52,8 +52,8 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 EnsurePsbuildInstalled
 
-exec { & dotnet restore .\src }
-exec { & dotnet build .\src -c Release }
+exec { & dotnet restore .\src\TransactionalFileManager.sln }
+exec { & dotnet build .\src\TransactionalFileManager.sln -c Release }
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
