b583891a81f24a6cc1b4916a924c9c337f40c61d
Mario te Best <mario.tebest@dccs.at>
2020-03-06 09:55:02 +0100

    updated build script

--- Build.ps1
+++ Build.ps1
@@ -52,8 +52,8 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 EnsurePsbuildInstalled
 
-exec { & dotnet restore }
-exec { & dotnet build -c Release }
+exec { & dotnet restore .\src}
+exec { & dotnet build .\src -c Release }
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
